ac3edaca3fe1db244d7589d2e2b09eb0873a5f2c
supardiputra <93508603+pardiputram@users.noreply.github.com>
2024-07-29 17:21:46 +0700

    add midleware

--- middlewares/jwtmiddleware.js
+++ middlewares/jwtmiddleware.js
@@ -1,22 +1,11 @@
 import axios from 'axios';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
 export const jwtMiddleware = async (req, res, next) => {
-  const token = req.headers['authorization']?.split(' ')[1]; // Ambil token dari header Authorization
-  
-  if (!token) {
-    return res.status(401).json({ message: 'Token not provided' });
-  }
-
+  const token = req.headers['authorization']?.split(' ')[1]; 
+  if (!token) return res.status(401).json({ message: 'Token not provided' })
   try {
-    // Verifikasi token dengan API otentikasi
     const response = await axios.post('http://localhost:3000/api/auth/verify', { token });
-
     if (response.data.valid) {
-      // Token valid, simpan informasi pengguna dalam req.user
-      req.user = response.data.user; // Simpan informasi pengguna jika diperlukan
+      req.user = response.data.user; 
       next();
     } else {
       res.status(401).json({ message: 'Invalid token' });
